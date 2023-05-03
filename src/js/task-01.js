7528cce7d596b8589bb8d00fa49d6b41a718b2e7
Vitalii Kozha <116120008+KozhaVitalii@users.noreply.github.com>
2023-05-03 16:52:27 +0300

    Added_first_part_of_hw

---
+++ src/js/task-01.js
@@ -0,0 +1,36 @@
+const categoriesEl = document.querySelectorAll('.item');
+console.log('Number of categories:', categoriesEl.length);
+
+categoriesEl.forEach(category => {
+    console.log('');
+    console.log(`Category: ${category.firstElementChild.textContent} `);
+    
+    // console.log(`Elements: ${category.lastElementChild.querySelectorAll('li').length} `);
+    
+    // or
+    
+    console.log(`Elements: ${category.querySelectorAll('li').length} `);
+    // console.log(`Elements: ${category.lastElementChild.textContent} `);
+});
+
+
+
+// 6.1 Напиши скрипт, який:
+
+// 1. Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// 2. Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст
+// заголовку елемента(тегу < h2 >) і кількість елементів в категорії(усіх < li >, вкладених в нього).
+
+// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+
+// В результаті, в консолі будуть виведені наступні повідомлення.
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,32 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
+// Ідентифікуєм ul в нашому HTML:
+const ingredientsList = document.querySelector('#ingredients');
+
+// створюємо масив: 
+const ingredientItems = ingredients.map((ingredient) => {
+  const listEl = document.createElement('li');
+  
+  listEl.textContent = ingredient;
+  listEl.classList.add('item');
+  
+  // console.log(listEl);
+
+  return listEl;
+});
+
+ingredientsList.append(...ingredientItems);
+console.log(ingredientsList);
+
+
